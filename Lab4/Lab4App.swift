805b4a98c72fc6c785bf89fec2c2b6fdd076360e
María Espejo <mayed92@hotmail.com>
2024-05-23 12:44:58 +0200

    Initial Commit

---
+++ Lab4/Lab4App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab4App.swift
+//  Lab4
+//
+//  Created by María Espejo on 23/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
