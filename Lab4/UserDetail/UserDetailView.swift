7b32a7ff85e0de396ba3111203b20afbae6279bc
María Espejo <mayed92@hotmail.com>
2024-05-30 20:17:09 +0200

    userdetail created

--- Lab4/UserDetail/UserDetailView.swift
+++ Lab4/UserDetail/UserDetailView.swift
@@ -8,13 +8,57 @@
 import SwiftUI
 
 struct UserDetailView: View {
+    @StateObject var viewModel: UserDetailViewModel
+    @State private var isEditing = false
+    
+    init(user: User) {
+        _viewModel = StateObject(wrappedValue: UserDetailViewModel(user: user))
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section(header: Text("Información del Usuario")) {
+                TextField("Nombre", text: $viewModel.user.name)
+                    .disabled(!isEditing)
+                TextField("Color Preferido", text: $viewModel.user.favoriteColor)
+                    .disabled(!isEditing)
+                DatePicker("Fecha de Nacimiento", selection: $viewModel.user.birthDate, displayedComponents: .date)
+                    .disabled(!isEditing)
+                TextField("Ciudad Favorita", text: $viewModel.user.favoriteCity)
+                    .disabled(!isEditing)
+                TextField("Número Favorito", value: $viewModel.user.favoriteNumber, formatter: NumberFormatter())
+                    .disabled(!isEditing)
+                TextField("Localización", text: $viewModel.user.location)
+                    .disabled(!isEditing)
+            }
+            
+            if isEditing {
+                Button("Guardar Cambios") {
+                    viewModel.updateUser(viewModel.user)
+                    isEditing = false
+                }
+            }
+            
+            NavigationLink(destination: MapView(viewModel: MapViewModel(city: viewModel.user.favoriteCity))) {
+                Text("Ver Ciudad en Mapa")
+                    .foregroundColor(.blue)
+            }
+        }
+        .navigationTitle("Detalles del Usuario")
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button(action: {
+                    isEditing.toggle()
+                }) {
+                    Text(isEditing ? "Cancelar" : "Editar")
+                }
+            }
+        }
     }
 }
 
 struct UserDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        UserDetailView()
+        UserDetailView(user: User(name: "John Doe", favoriteColor: "Azul", birthDate: Date(), favoriteCity: "Madrid", favoriteNumber: 7, location: "40.416775, -3.703790"))
     }
 }
--- Lab4/UserDetail/UserDetailViewModel.swift
+++ Lab4/UserDetail/UserDetailViewModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import Combine
+
+class UserDetailViewModel: ObservableObject {
+    @Published var user: User
+    
+    private let userDefaultsService: UserDefaultsService
+    
+    init(user: User, userDefaultsService: UserDefaultsService = UserDefaultsService()) {
+        self.user = user
+        self.userDefaultsService = userDefaultsService
+    }
+    
+    func updateUser(_ updatedUser: User) {
+        var users = userDefaultsService.loadUsers()
+        if let index = users.firstIndex(where: { $0.id == updatedUser.id }) {
+            users[index] = updatedUser
+            userDefaultsService.saveUsers(users)
+        }
+    }
+}
