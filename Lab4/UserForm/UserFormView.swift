4c60200ade7c89eff69cdcd526756bac8d9d900a
María Espejo <mayed92@hotmail.com>
2024-05-30 20:16:54 +0200

    userform created

--- Lab4/UserForm/UserFormView.swift
+++ Lab4/UserForm/UserFormView.swift
@@ -8,13 +8,39 @@
 import SwiftUI
 
 struct UserFormView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var viewModel: UserFormViewModel
+    var completion: (() -> Void)?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Section(header: Text("Información del Usuario")) {
+                    TextField("Nombre", text: $viewModel.name)
+                    TextField("Color Preferido", text: $viewModel.favoriteColor)
+                    DatePicker("Fecha de Nacimiento", selection: $viewModel.birthDate, displayedComponents: .date)
+                    TextField("Ciudad Favorita", text: $viewModel.favoriteCity)
+                    TextField("Número Favorito", value: $viewModel.favoriteNumber, formatter: NumberFormatter())
+                    TextField("Localización Actual", text: $viewModel.location)
+                    
+                    Button("Obtener Localización Actual") {
+                        viewModel.fetchCurrentLocation()
+                    }
+                }
+                
+                Button("Guardar") {
+                    viewModel.saveUser()
+                    presentationMode.wrappedValue.dismiss()
+                    completion?()
+                }
+            }
+            .navigationTitle("Añadir Usuario")
+        }
     }
 }
 
 struct UserFormView_Previews: PreviewProvider {
     static var previews: some View {
-        UserFormView()
+        UserFormView(viewModel: UserFormViewModel())
     }
 }
--- Lab4/UserForm/UserFormViewModel.swift
+++ Lab4/UserForm/UserFormViewModel.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import Combine
+
+class UserFormViewModel: ObservableObject {
+    @Published var name = ""
+    @Published var favoriteColor = ""
+    @Published var birthDate = Date()
+    @Published var favoriteCity = ""
+    @Published var favoriteNumber = 0
+    @Published var location = ""
+    var completion: (() -> Void)?
+    
+    private let userDefaultsService: UserDefaultsService
+    private let localizationService: LocalizationService
+    
+    init(userDefaultsService: UserDefaultsService = UserDefaultsService(), localizationService: LocalizationService = LocalizationService()) {
+        self.userDefaultsService = userDefaultsService
+        self.localizationService = localizationService
+    }
+    
+    func fetchCurrentLocation() {
+        localizationService.requestLocation { [weak self] location in
+            self?.location = location
+        }
+    }
+    
+    func saveUser() {
+        let newUser = User(name: name, favoriteColor: favoriteColor, birthDate: birthDate, favoriteCity: favoriteCity, favoriteNumber: favoriteNumber, location: location)
+        var users = userDefaultsService.loadUsers()
+        users.append(newUser)
+        userDefaultsService.saveUsers(users)
+        completion?()
+    }
+}
